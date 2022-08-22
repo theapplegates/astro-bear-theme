d623a1732a65b6e46a0732fc44d281934e329f79
Paul <paul@theapplegates.com>
2022-08-22 19:46:00 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import { Picture } from '@astrojs/image/components';
+import localImage from '/assets/Gulfstream-G400.jpg';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
